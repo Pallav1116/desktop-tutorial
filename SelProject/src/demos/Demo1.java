b3bf082d4af2050d1187f9fc66eb96b7c11e9474
Pallav1116 <81137847+Pallav1116@users.noreply.github.com>
2021-03-22 11:11:31 +0530

    Add files via upload

---
+++ SelProject/src/demos/Demo1.java
@@ -0,0 +1,13 @@
+package demos;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Asus\\Desktop\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+	}
+
+}
